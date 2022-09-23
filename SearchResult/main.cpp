8fbb5e5e8a85af7e8d4d55cd39f8ba387c3dce1c
resting3 <88263665+resting3@users.noreply.github.com>
2022-09-23 18:25:26 +0200

    Update main.cpp

--- SearchResult/main.cpp
+++ SearchResult/main.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <cstddef>
 
+#define R
+
 using namespace std;
 
 
@@ -12,6 +14,7 @@ using namespace std;
 void leggi(string utente){
     cout << "Una produzione di Resting" << endl;
     cout << "Il bot sta cercando..." << endl;
+    
     while (true){
         char * user = getenv("username");
         string contenitore;
@@ -21,16 +24,16 @@ void leggi(string utente){
         string adress = "C:/Users/" + string(user) + "/Appdata/Roaming/.minecraft/logs/";
         file.open(adress + "latest.log");
         if (file.is_open(), ios::in){
-        
+
         string   a;
         //  file >> stringapass;
         // cout << stringapass;
         // il ciclo for while è migliore per la lettura di un intero file il ciclo for è abbastanza obsoleto per questa operazione.
             while(getline(file, contenitore)){ //read data from file object and put it into string.
-            
+
                 if (contenitore.find(utente) != std::string::npos) {
                     ofstream filew;
-                    filew.open("Result.txt", ios_base::app);
+                    filew.open("Risultato.txt", ios_base::app);
                     filew << "\n" << contenitore;
                     cout << "\nIl bot ha trovato qualcosa!";
                     filew.close();
@@ -39,19 +42,19 @@ void leggi(string utente){
                     filewe << "\n" << " ";
                     filewe.close();
 
-                    
+
                 }
             }
 
-        
-        
-                
+
+
+
 
 
             // vettore.push_back(contenitore); //aggiungere il contenitore nel vettore tipo stringa
-        
 
-                
+
+
 
             }
 
@@ -62,13 +65,13 @@ void leggi(string utente){
 
 
 
-        //for (vector<string>::iterator it = vettore.begin(); it != vettore.end(); it++){ //Questo è un bel bordello per poter fare campionamento di una singola stringa bisogna dichiarare un vettore far passare il contenitore dove i dati andranno nel vettore poi il vettore viene impiegato inun singolo for di controllo in cui  vi è il pointer iterator che permette la lettura 
+        //for (vector<string>::iterator it = vettore.begin(); it != vettore.end(); it++){ //Questo è un bel bordello per poter fare campionamento di una singola stringa bisogna dichiarare un vettore far passare il contenitore dove i dati andranno nel vettore poi il vettore viene impiegato inun singolo for di controllo in cui  vi è il pointer iterator che permette la lettura
             //   cout << *it << ' ';
             // cout << endl;
                 //if (*it == "c"){
                 //    cout << *it;
                 //}
-            
+
 
 
 
@@ -79,10 +82,10 @@ void leggi(string utente){
 
         }
 }
-       
 
 
-    
+
+
 
 
 //void scrivi(string dato){
@@ -100,45 +103,15 @@ void leggi(string utente){
 int main()
 {
     while (true){
-        string input; 
+        string input;
         string fetched;
-            cout << "Inserire codice di accesso: ";
-            cin >> input; 
-            if (input == "sasso"){
-                cout << "Inserire nome di prefisso: ";
-                cin >> fetched; 
-                cout << "Parola chiave inserita: " << fetched << endl;
-                leggi(fetched);
-
-            }
-            else{
-                cout << "Password Sbagliata!\n";
-            }
-
-                        
+        cout << "Inserire nome di prefisso: ";
+        cin >> fetched;
+        cout << "Parola chiave inserita: " << fetched << endl;
+        leggi(fetched);
 
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     return 0;
